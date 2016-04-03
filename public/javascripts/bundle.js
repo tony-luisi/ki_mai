c9de2184385fb6b3a0ead73d4d405d64dd1f04e5
tony-luisi <t.luisi@gmail.com>
2016-04-03 16:11:19 +1200

    updated

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -9850,14 +9850,18 @@ $('form').submit(function(){
   var message = $('#m').val()
   console.log(message)
   socket.emit('message', { message: message })
+  $('#m').val('');
   return false
 })
-
-console.log('hi')
 
 socket.on('message', function (data) {
   console.log('received', data);
   $('#messages').append($('<li>').text(data.message))
-});
+})
+
+$('#m').on('keyup', function() {
+  var message = $('#m').val()
+  console.log(message)
+})
 
 },{"jquery":1}]},{},[2]);
--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ $('form').submit(function(){
   var message = $('#m').val()
   console.log(message)
   socket.emit('message', { message: message })
+  $('#m').val('');
   return false
 })
-
-console.log('hi')
 
 socket.on('message', function (data) {
   console.log('received', data);
   $('#messages').append($('<li>').text(data.message))
-});
+})
+
+$('#m').on('keyup', function() {
+  var message = $('#m').val()
+  console.log(message)
+})
